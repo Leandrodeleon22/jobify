52505a349154c19c4d42735bc04972c88e5c4dad
Leandrodeleon22 <leandrodeleon22@gmail.com>
2023-02-25 10:25:54 -0500

    final

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,10 +1,17 @@
 import express from "express";
 import { login, register, updateUser } from "../controllers/authController.js";
 import authenticateUser from "../middleware/auth.js";
-const router = express.Router();
+import { rateLimit } from "express-rate-limit";
 
-router.route("/register").post(register);
-router.route("/login").post(login);
+const router = express.Router();
+const apiLimiter = rateLimit({
+  windowMs: 15 * 60 * 1000,
+  max: 10,
+  message: " Too many requests from this IP, please try again after 15 minutes",
+});
+
+router.route("/register").post(apiLimiter, register);
+router.route("/login").post(apiLimiter, login);
 router.route("/updateUser").patch(authenticateUser, updateUser);
 
 export default router;
--- server.js
+++ server.js
@@ -15,9 +15,24 @@ import errorHandlerMiddleware from "./middleware/error-handler.js";
 import connectDB from "./db/connect.js";
 import morgan from "morgan";
 import authenticateUser from "./middleware/auth.js";
+import { dirname } from "path";
+import { fileURLToPath } from "url";
+import path from "path";
+import helmet from "helmet";
+import xss from "xss-clean";
+
+import mongoSanitize from "express-mongo-sanitize";
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
+app.use(express.static(path.resolve(__dirname, "./client/build")));
 
 // app.use(cors());
 app.use(express.json());
+app.use(helmet());
+app.use(xss());
+app.use(mongoSanitize());
+
 if (process.env.NODE_ENV !== "production") {
   app.use(morgan("dev"));
 }
@@ -27,6 +42,10 @@ app.get("/api/v1", (req, res) => {
 
 app.use("/api/v1/auth", authRouter);
 app.use("/api/v1/jobs", authenticateUser, jobRoutes);
+
+app.get("*", function (request, response) {
+  response.sendFile(path.resolve(__dirname, "./client/build", "index.html"));
+});
 
 app.use(notFoundMiddleware);
 app.use(errorHandlerMiddleware);
