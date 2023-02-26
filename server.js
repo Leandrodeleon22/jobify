21d7966067cd62bea152b2a177c817618756354f
Leandrodeleon22 <leandrodeleon22@gmail.com>
2023-02-25 21:48:57 -0500

    deploy refine

--- server.js
+++ server.js
@@ -2,7 +2,7 @@
 import express from "express";
 import "express-async-errors";
 import dotenv from "dotenv";
-const path = require("path");
+// const path = require("path");
 const app = express();
 
 dotenv.config();
@@ -24,7 +24,7 @@ import xss from "xss-clean";
 
 import mongoSanitize from "express-mongo-sanitize";
 
-// const __dirname = dirname(fileURLToPath(import.meta.url));
+const __dirname = dirname(fileURLToPath(import.meta.url));
 
 // app.use(express.static(path.resolve(__dirname, "./client/build")));
 
@@ -34,9 +34,13 @@ app.use(helmet());
 app.use(xss());
 app.use(mongoSanitize());
 
+// app.get("/", function (req, res) {
+//   res.sendFile(path.join(__dirname, "build", "index.html"));
+// });
+
 app.use(express.static(path.join(__dirname, "build")));
-app.get("/", function (req, res) {
-  res.sendFile(path.join(__dirname, "build", "index.html"));
+app.get("*", function (request, response) {
+  response.sendFile(path.resolve(__dirname, "./client/build", "index.html"));
 });
 
 if (process.env.NODE_ENV !== "production") {
@@ -48,10 +52,6 @@ app.get("/api/v1", (req, res) => {
 
 app.use("/api/v1/auth", authRouter);
 app.use("/api/v1/jobs", authenticateUser, jobRoutes);
-
-app.get("*", function (request, response) {
-  response.sendFile(path.resolve(__dirname, "./client/build", "index.html"));
-});
 
 app.use(notFoundMiddleware);
 app.use(errorHandlerMiddleware);
