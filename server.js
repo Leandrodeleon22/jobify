9b448905f0c1526765a23d1ae03c54b9bfb24d47
Leandrodeleon22 <leandrodeleon22@gmail.com>
2023-02-25 22:04:39 -0500

    deploy render

--- server.js
+++ server.js
@@ -24,7 +24,7 @@ import xss from "xss-clean";
 
 import mongoSanitize from "express-mongo-sanitize";
 
-const __dirname = dirname(fileURLToPath(import.meta.url));
+// const __dirname = dirname(fileURLToPath(import.meta.url));
 
 // app.use(express.static(path.resolve(__dirname, "./client/build")));
 
@@ -38,10 +38,10 @@ app.use(mongoSanitize());
 //   res.sendFile(path.join(__dirname, "build", "index.html"));
 // });
 
-app.use(express.static(path.join(__dirname, "build")));
-app.get("*", function (request, response) {
-  response.sendFile(path.resolve(__dirname, "./client/build", "index.html"));
-});
+// app.use(express.static(path.join(__dirname, "build")));
+// app.get("*", function (request, response) {
+//   response.sendFile(path.resolve(__dirname, "./client/build", "index.html"));
+// });
 
 if (process.env.NODE_ENV !== "production") {
   app.use(morgan("dev"));
