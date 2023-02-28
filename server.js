357ea1a73815ef1b015c171a81f37dadf1f86cca
Leandrodeleon22 <leandrodeleon22@gmail.com>
2023-02-28 05:24:32 -0500

    deploy fix

--- server.js
+++ server.js
@@ -2,6 +2,7 @@
 import express from "express";
 import "express-async-errors";
 import dotenv from "dotenv";
+import cors from "cors";
 // const path = require("path");
 const app = express();
 
@@ -28,7 +29,15 @@ import mongoSanitize from "express-mongo-sanitize";
 
 // app.use(express.static(path.resolve(__dirname, "./client/build")));
 
-// app.use(cors());
+app.use(cors());
+
+// app.use((req, res, next) => {
+//   res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
+//   res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+//   res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+//   next();
+// });
+
 app.use(express.json());
 app.use(helmet());
 app.use(xss());
@@ -38,14 +47,17 @@ app.use(mongoSanitize());
 //   res.sendFile(path.join(__dirname, "build", "index.html"));
 // });
 
-// app.use(express.static(path.join(__dirname, "build")));
-// app.get("*", function (request, response) {
-//   response.sendFile(path.resolve(__dirname, "./client/build", "index.html"));
-// });
-
 if (process.env.NODE_ENV !== "production") {
   app.use(morgan("dev"));
 }
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "build")));
+  app.get("*", function (request, response) {
+    response.sendFile(path.resolve(__dirname, "./client/build", "index.html"));
+  });
+}
+
 app.get("/api/v1", (req, res) => {
   res.send({ msg: "Welcome" });
 });
